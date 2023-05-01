5d22a41858e0e9bf3e710d861a2807fffbbce5e3
ct197-waikato <ct197@students.waikato.ac.nz>
2023-05-02 00:36:10 +1200

    H2

--- mega-array/src/main/java/ictgradschool/industry/homework/mega_array/MegaArray.java
+++ mega-array/src/main/java/ictgradschool/industry/homework/mega_array/MegaArray.java
@@ -22,15 +22,17 @@ public class MegaArray {
 
     public void addItem(String item) {
         if (this.head + 1 == this.data.length) {
-            String[] newData = new String[this.data.length * 2];
-            for (int i = 0; i < this.data.length; i++) {
-                newData[i] = this.data[i];
-            }
-            this.data = newData;
+            grow();
         }
         this.data[head++] = item;
     }
-
+    private void grow() {
+        String[] newData = new String[this.data.length * 2];
+        for (int i = 0; i < this.data.length; i++) {
+            newData[i] = this.data[i];
+        }
+        this.data = newData;
+    }
     public int indexOf(String item) {
         for (int i = 0; i < this.head; i++) {
             if (this.data[i].equals(item)) {
